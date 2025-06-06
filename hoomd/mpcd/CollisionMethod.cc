647ebb950ab690c8595cb7ac577a911db22f941f
mzbush <107584654+mzbush@users.noreply.github.com>
2025-06-06 12:04:27 -0500

    Update hoomd/mpcd/CollisionMethod.cc
    Co-authored-by: Michael Howard <mphoward@auburn.edu>

--- hoomd/mpcd/CollisionMethod.cc
+++ hoomd/mpcd/CollisionMethod.cc
@@ -384,7 +384,7 @@ void mpcd::CollisionMethod::transferRigidBodyMomenta(uint64_t timestep)
 
         // compute initial kinetic energy
         Scalar ke_tra_change
-            = -0.5 * mass
+            = -Scalar(0.5) * mass
               * (vel_mass.x * vel_mass.x + vel_mass.y * vel_mass.y + vel_mass.z * vel_mass.z);
         // compute and store change in velocity
         if (mass > 0)
