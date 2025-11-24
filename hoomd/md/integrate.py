03cf5ed682a992f63800aac30b3209b969b4366f
Jon <jon.aronoff@utah.edu>
2025-11-24 14:04:37 -0700

    update integrator docs

--- hoomd/md/integrate.py
+++ hoomd/md/integrate.py
@@ -239,15 +239,14 @@ class Integrator(_DynamicIntegrator):
 
     - `hoomd.md.constrain`
 
-    Examples::
+    .. code-block:: python
+    
+        nve = hoomd.md.methods.ConstantVolume(
+            filter=hoomd.filter.All(), 
+            thermostat=None,
+            )
+        integrator.methods.append (nve)
 
-        nlist = hoomd.md.nlist.Cell()
-        lj = hoomd.md.pair.LJ(nlist=nlist)
-        lj.params.default = dict(epsilon=1.0, sigma=1.0)
-        lj.r_cut[('A', 'A')] = 2**(1/6)
-        nve = hoomd.md.methods.NVE(filter=hoomd.filter.All())
-        integrator = hoomd.md.Integrator(dt=0.001, methods=[nve], forces=[lj])
-        sim.operations.integrator = integrator
 
     {inherited}
 
